405cdd1792ba3f45a1e7a600df5f47acfebea11b
elvischepin-github <hello.elvis@icloud.com>
2025-04-25 17:31:04 +0300

    csv export testing

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
-const { createInterface } = require("node:readline");
-const readline = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: ">>> ",
+const { convertArrayToCSV } = require("convert-array-to-csv");
+const fileSystem = require("node:fs");
+
+const csvFile = [];
+
+const testData = [
+  {
+    Line: 1,
+    Reward_Date: "11/15/20",
+    Investment_Amount: "10.000000",
+    Current_Month_Reward_Amount: "0.009589",
+    Total_Reward_Amount_To_Date: "0.009589",
+    Staking_Reward_Rate: "7.00%",
+  },
+];
+const headers = Object.keys(testData[0]);
+
+csvFile.push(headers);
+
+testData.forEach((item) => {
+  csvFile.push(Object.values(item));
 });
 
-readline.prompt();
+const csvFromArrayOfArrays = convertArrayToCSV(csvFile, {
+  headers,
+  separator: ";",
+});
 
-readline
-  .on("line", (line) => {
-    console.log(line);
-
-    process.exit(0);
-  })
-  .on("close", () => {
-    console.log("Process ended, exiting.");
-  });
+fileSystem.writeFile("output.csv", csvFromArrayOfArrays, (err) => {
+  if (err) {
+    console.log("❌ Error:", err);
+  }
+  console.log("CSV file saved successfully!");
+});
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Simplified Ethereum Staking Profit Calculator
+
+## Instructions
+
+**Environment Setup**
+
+Use links below, select your operating system, download files and install them into your computer.
+
+- Node.js: https://nodejs.org/en/download
+- Visual Studio Code: https://code.visualstudio.com/download
